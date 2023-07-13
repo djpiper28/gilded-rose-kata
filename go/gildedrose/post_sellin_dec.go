753648fd8f691c6673796179579375e04cd9b751
Danny Piper <danny.piper@bjss.com>
2023-07-13 11:15:11 +0100

    chore (refactor): move pre and post sellin dec updates into new units

---
+++ go/gildedrose/post_sellin_dec.go
@@ -0,0 +1,29 @@
+package gildedrose
+
+func updateBrieQualityPostSellInDec(item *Item) {
+	if item.Quality < 50 {
+		item.Quality = item.Quality + 1
+	}
+}
+
+func updateItemQualityPostSellInDec(item *Item) {
+	if item.SellIn >= 0 {
+		return
+	}
+
+	if item.Name == AGED_BRIE {
+		updateBrieQualityPostSellInDec(item)
+		return
+	}
+
+	if item.Name != BACKSTAGE_PASS {
+		if item.Quality > 0 {
+			if item.Name != SULFURAS {
+				item.Quality = item.Quality - 1
+			}
+		}
+		return
+	}
+
+	item.Quality = item.Quality - item.Quality
+}
---
+++ go/gildedrose/pre_sellin_dec.go
@@ -0,0 +1,38 @@
+package gildedrose
+
+func updateBackStagePassQualityPreSellInDec(item *Item) {
+	if item.Name != BACKSTAGE_PASS {
+		return
+	}
+
+	if item.SellIn < 11 {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	}
+	if item.SellIn < 6 {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	}
+}
+
+func updateStandardItemQualityPreSellInDec(item *Item) {
+	if item.Quality > 0 {
+		if item.Name != SULFURAS {
+			item.Quality = item.Quality - 1
+		}
+	}
+}
+
+func updateItemQualityPreSellInDec(item *Item) {
+	if item.Name != AGED_BRIE && item.Name != BACKSTAGE_PASS {
+		updateStandardItemQualityPreSellInDec(item)
+		return
+	}
+
+	if item.Quality < 50 {
+		item.Quality = item.Quality + 1
+		updateBackStagePassQualityPreSellInDec(item)
+	}
+}
