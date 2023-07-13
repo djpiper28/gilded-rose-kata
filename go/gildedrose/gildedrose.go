db23d0266dcf7b6c90514389a0c1f0040ea1393c
Danny Piper <danny.piper@bjss.com>
2023-07-13 11:05:01 +0100

    chore (refactor): moved post sellin dec quality update to a new function

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -32,6 +32,27 @@ func updateItemQuality(item *Item) {
 	}
 }
 
+func updateItemQualityPostSellInDec(item *Item) {
+	if item.SellIn >= 0 {
+		return
+	}
+	if item.Name != AGED_BRIE {
+		if item.Name != BACKSTAGE_PASS {
+			if item.Quality > 0 {
+				if item.Name != SULFURAS {
+					item.Quality = item.Quality - 1
+				}
+			}
+		} else {
+			item.Quality = item.Quality - item.Quality
+		}
+	} else {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	}
+}
+
 func updateItem(item *Item) {
 	updateItemQuality(item)
 
@@ -39,23 +60,7 @@ func updateItem(item *Item) {
 		item.SellIn = item.SellIn - 1
 	}
 
-	if item.SellIn < 0 {
-		if item.Name != AGED_BRIE {
-			if item.Name != BACKSTAGE_PASS {
-				if item.Quality > 0 {
-					if item.Name != SULFURAS {
-						item.Quality = item.Quality - 1
-					}
-				}
-			} else {
-				item.Quality = item.Quality - item.Quality
-			}
-		} else {
-			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-			}
-		}
-	}
+	updateItemQualityPostSellInDec(item)
 }
 
 func UpdateQuality(items []*Item) {
