68251e06739215686ba222989ef8396b358b8814
Danny Piper <danny.piper@bjss.com>
2023-07-13 15:40:54 +0100

    chore (refactor): move sulfuras check to guard statement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,6 +12,8 @@ class GildedRose {
     }
 
     private void updateItemQuality(int i) {
+        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) return;
+
         if (items[i].name.equals("Aged Brie")
             || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (items[i].quality < 50) {
@@ -39,9 +41,7 @@ class GildedRose {
             }
         }
 
-        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
+        items[i].sellIn = items[i].sellIn - 1;
 
         if (items[i].sellIn < 0) {
             if (!items[i].name.equals("Aged Brie")) {
