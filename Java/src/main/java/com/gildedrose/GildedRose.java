ebdd779920f0dabdbf65ecd2b737fd3dc89560fc
Danny Piper <danny.piper@bjss.com>
2023-07-13 15:46:12 +0100

    chore (refactor): moved presellindec backstage pass

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -26,22 +26,26 @@ class GildedRose {
                 items[i].quality = items[i].quality + 1;
 
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+                    updateBackstagePassPreSellInDecrement(i);
                 }
             }
         } else {
             if (items[i].quality > 0) {
                 items[i].quality = items[i].quality - 1;
+            }
+        }
+    }
+
+    private void updateBackstagePassPreSellInDecrement(int i) {
+        if (items[i].sellIn < 11) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
+            }
+        }
+
+        if (items[i].sellIn < 6) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
             }
         }
     }
