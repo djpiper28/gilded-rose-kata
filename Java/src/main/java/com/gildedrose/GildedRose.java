a2107c6de096aeb838c33d2b3faedf2ead83d3e7
Danny Piper <danny.piper@bjss.com>
2023-07-13 15:41:33 +0100

    chore (refactor): invert if statement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -44,7 +44,11 @@ class GildedRose {
         items[i].sellIn = items[i].sellIn - 1;
 
         if (items[i].sellIn < 0) {
-            if (!items[i].name.equals("Aged Brie")) {
+            if (items[i].name.equals("Aged Brie")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            } else {
                 if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (items[i].quality > 0) {
                         if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -53,10 +57,6 @@ class GildedRose {
                     }
                 } else {
                     items[i].quality = 0;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
                 }
             }
         }
