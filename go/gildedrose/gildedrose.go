0c64a0f9f7e47b4a2dc1db27c7345d91eeea7104
Danny Piper <danny.piper@bjss.com>
2023-07-14 12:49:00 +0100

    chore (refactor): inlined a method

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -2,12 +2,8 @@ package gildedrose
 
 import "github.com/emilybache/gildedrose-refactoring-kata/gildedrose/gildeditems"
 
-func updateItem(item *gildeditems.Item) {
-	item.GetItemStrategy().DoItemUpdate()
-}
-
 func UpdateQuality(items []*gildeditems.Item) {
 	for _, item := range items {
-		updateItem(item)
+		item.GetItemStrategy().DoItemUpdate()
 	}
 }
