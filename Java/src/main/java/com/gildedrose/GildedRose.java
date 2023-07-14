4d250a23f034ddfec322de3fc0b233b9580d9030
Danny Piper <danny.piper@bjss.com>
2023-07-14 10:04:09 +0100

    Revert "chore (refactor): use arraylist instead of an array"
    This reverts commit 40851274dbe4c8717d1b9df628915d7a57f60719.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,15 +3,13 @@ package com.gildedrose;
 import com.gildedrose.Items.GildedItem;
 import com.gildedrose.Items.GildedItemFactory;
 
-import java.util.LinkedList;
-import java.util.List;
-
 class GildedRose {
-    final private List<GildedItem> items = new LinkedList<>();
+    final GildedItem[] items;
 
     public GildedRose(Item[] items) {
-        for (final Item item : items) {
-            this.items.add(GildedItemFactory.from(item));
+        this.items = new GildedItem[items.length];
+        for (int i = 0; i < items.length; i++) {
+            this.items[i] = GildedItemFactory.from(items[i]);
         }
     }
 
